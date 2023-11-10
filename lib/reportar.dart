0a3b17272830146abd8babe62da7cb487f5dbd57
Evanrada <marycarmenhr@gmail.com>
2023-11-10 13:42:24 -0600

    estructura1

--- lib/reportar.dart
+++ lib/reportar.dart
@@ -26,3 +26,135 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class reportar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mapa Geoglamour',
+            style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
+        backgroundColor: Colors.black,
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            );
+          },
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.black,
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    'GeoGlamour',
+                    style: TextStyle(fontSize: 30, color: Colors.white),
+                  ),
+                  Image.asset(
+                    'assets/logo2.png',
+                    width: 100, // Ajusta el ancho según tus preferencias
+                    height: 98, // Ajusta la altura según tus preferencias
+                  ), //
+                ],
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.map),
+              title: Text(
+                'Mapa',
+                style: TextStyle(fontSize: 20),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MapScreen()),
+                ); // Cierra el Drawer
+                // Agrega la lógica para navegar a la página de inicio aquí
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.diamond_rounded),
+              title: Text(
+                'Accesorios',
+                style: TextStyle(fontSize: 20),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => accesorios()),
+                ); // Cierra el Drawer
+                // Agrega la lógica para navegar a la página "Acerca de" aquí
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.report),
+              title: Text(
+                'Reportar',
+                style: TextStyle(fontSize: 20),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => reportar()),
+                ); // Cierra el Drawer
+                // Agrega la lógica para navegar a la página "Acerca de" aquí
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.exit_to_app),
+              title: Text(
+                'Cerrar sesión',
+                style: TextStyle(
+                  color: Colors.red,
+                  // Cambia el color del texto a rojo
+                  fontSize: 20.0,
+                  // Cambia el tamaño de la fuente según tus preferencias
+                  fontWeight: FontWeight
+                      .bold, // Cambia el peso de la fuente según tus preferencias
+                ),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginScreen()),
+                ); // Cierra el Drawer
+                // Agrega la lógica para navegar a la página "Acerca de" aquí
+              },
+            ),
+            ListTile(
+              title: Text(
+                'Pruebaaaa',
+                style: TextStyle(
+                  color: Colors.red,
+                  // Cambia el color del texto a rojo
+                  fontSize: 20.0,
+                  // Cambia el tamaño de la fuente según tus preferencias
+                  fontWeight: FontWeight
+                      .bold, // Cambia el peso de la fuente según tus preferencias
+                ),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MapScreen2()),
+                ); // Cierra el Drawer
+                // Agrega la lógica para navegar a la página "Acerca de" aquí
+              },
+            ),
+          ],
+        ),
+      ),
+      body: ReportAccessoryForm(),
+    );
+  }
+}
