5257c404a4af6c0c4b0aa5213276f5aa9a44fbb6
Evanrada <marycarmenhr@gmail.com>
2023-11-10 13:36:47 -0600

    reportar1

---
+++ lib/reportar.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:emprendimiento/main.dart';
+
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: reportar(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.deepOrange, // Color de fondo de la AppBar
+        appBarTheme: AppBarTheme(
+          color: Colors.deepOrange, // Color de fondo de la AppBar
+        ),
+      ),
+      home: accesorios(),
+    );
+  }
+}
